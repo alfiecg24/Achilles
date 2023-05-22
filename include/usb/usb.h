9e46689886edac13e73b31666a72eb6bfb4aeb61
alfiecg24 <alfiecockell@outlook.com>
2023-05-22 10:33:49 +0100

    Ensure exploit() is called once device is in DFU mode

--- include/usb/usb.h
+++ include/usb/usb.h
@@ -9,9 +9,5 @@
 #include <CoreFoundation/CoreFoundation.h>
 
 char *getDeviceSerialNumber(io_service_t device);
- // building throws a unknown type name 'device_t' error
-/*
-this is because device_t is defined in device.h, but device.h is not included in usb_utils.h
-*/
 
 #endif // USB_UTILS_H
