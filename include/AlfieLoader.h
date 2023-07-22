c2b2d84ac022d404f1fb27b0d2fdfbf5b8493ffd
alfiecg24 <alfiecockell@outlook.com>
2023-07-22 21:39:52 +0100

    Add some documentation to header file

--- include/AlfieLoader.h
+++ include/AlfieLoader.h
@@ -44,6 +44,13 @@ typedef struct
     };
 } arg_t;
 
+extern arg_t args[];
+
+extern uint16_t cpid;
+extern size_t config_hole, ttbr0_vrom_off, ttbr0_sram_off, config_large_leak, config_overwrite_pad;
+extern uint64_t tlbi, nop_gadget, ret_gadget, patch_addr, ttbr0_addr, func_gadget, write_ttbr0, memcpy_addr, aes_crypto_cmd, boot_tramp_end, gUSBSerialNumber, dfu_handle_request, usb_core_do_transfer, dfu_handle_bus_reset, insecure_memory_base, handle_interface_request, usb_create_string_descriptor, usb_serial_number_string_descriptor;
+
+
 // ******************************************************
 // Function: getArgumentByName()
 //
@@ -56,12 +63,5 @@ typedef struct
 //      arg_t *: the arg_t struct for the argument, NULL if the argument doesn't exist
 // ******************************************************
 arg_t *getArgumentByName(char *name);
-
-extern arg_t args[];
-
-extern uint16_t cpid;
-extern size_t config_hole, ttbr0_vrom_off, ttbr0_sram_off, config_large_leak, config_overwrite_pad;
-extern uint64_t tlbi, nop_gadget, ret_gadget, patch_addr, ttbr0_addr, func_gadget, write_ttbr0, memcpy_addr, aes_crypto_cmd, boot_tramp_end, gUSBSerialNumber, dfu_handle_request, usb_core_do_transfer, dfu_handle_bus_reset, insecure_memory_base, handle_interface_request, usb_create_string_descriptor, usb_serial_number_string_descriptor;
-
 
 #endif // ALFIELOADER_H
--- include/boot/pongo/pongo.h
+++ include/boot/pongo/pongo.h
@@ -7,7 +7,30 @@
 #include <exploit/dfu.h>
 #include <time.h>
 
+// ******************************************************
+// Function: bootPongoOS()
+//
+// Purpose: Boot PongoOS on a device in YoloDFU mode
+//
+// Parameters:
+//      device_t *device: the device to boot PongoOS on
+//
+// Returns:
+//      bool: true if PongoOS was booted successfully, false otherwise
+// ******************************************************
 bool bootPongoOS(device_t *device);
+
+// ******************************************************
+// Function: isInPongoOS()
+//
+// Purpose: Checks if a USB serial number indicates a device that is in PongoOS
+//
+// Parameters:
+//      char *serial: the serial number of the device
+//
+// Returns:
+//      bool: true if the device is in PongoOS, false otherwise
+// ******************************************************
 bool isInPongoOS(char *serial);
 
 #endif // PONGO_H
